4b626e0cb8c39050a6d18f85f2f5c5abde67a36b
ballsona0925 <ballsona0925@naver.com>
2022-03-10 23:53:11 +0900

    react practice(async2)

--- typescript-todolist/src/index.tsx
+++ typescript-todolist/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from "./react_practice/App_contextVer";
-import App from './App';
+// import App from './App';
+import User from './react_practice/redux/User_reducerVer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<User />, document.getElementById('root'));
---
+++ typescript-todolist/src/react_practice/redux/User.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+const API = 'https://jsonplaceholder.typicode.com/users';
+
+export default function User() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(API);
+      const data = await res.json();
+      setUsers(data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+  return (
+    <>
+      {loading ? (
+        <h1>로딩중</h1>
+      ) : (
+        <div>
+          {users.map((user) => {
+            return <div key={user.id}>{user.name}</div>;
+          })}
+        </div>
+      )}
+    </>
+  );
+}
